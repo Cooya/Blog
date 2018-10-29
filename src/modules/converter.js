aec8e044b27933528d80656e97408f633d8c4c70
Nicolas <nico@cooya.fr>
2018-10-29 16:14:27 +1300

    src folder created + converter module added + dynamic markdown converting

---
+++ src/modules/converter.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const path = require('path');
+const showdown = require('showdown');
+const util = require('util');
+
+const config = require('../../config');
+const gallery = require('./gallery_extension');
+
+const readDir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const fileStat = util.promisify(fs.stat);
+
+module.exports = class Converter {
+    constructor(srcMarkdownFolder, destHtmlFolder) {
+        this.srcMarkdownFolder = srcMarkdownFolder;
+        this.destHtmlFolder = destHtmlFolder;
+        this.posts = {};
+        gallery.load(config.picturesFolderUrl, config.pictureWidths);
+    }
+
+    async getPost(postId) {
+        // get the post object
+        const post = this.posts[postId];
+        if(!post)
+            return null;
+        //console.debug(post);
+        
+        // check the save modification date with the current file modification date 
+        const lastModificationDate = await getFileLastModificationDate(post.markdown_file_path);
+        //console.debug(lastModificationDate, post.last_modification_date);
+        if(lastModificationDate.getTime() != post.last_modification_date.getTime())
+            await convertFile.call(this, postId + '.md'); // refresh the html file
+
+        return post;
+    }
+
+    async convertFiles() {
+        (await readDir(this.srcMarkdownFolder)).forEach(convertFile.bind(this));
+    }
+};
+
+async function convertFile(fileName) {
+    if (path.extname(fileName) != '.md')
+        return;
+    console.debug('Converting file "' + fileName + '"...');
+
+    // we create a new post object
+    const postId = fileName.replace('.md', '');
+    const post = {markdown_file_path: this.srcMarkdownFolder + fileName};
+
+    // we read the file, extract its header and save it into the post object
+    let fileContent = (await readFile(post.markdown_file_path)).toString();
+    const postDataAndContent = extractHeaderFromPost(fileContent);
+    if (postDataAndContent.header) {
+        fileContent = postDataAndContent.content; // file content whithout header
+        for(let attr in postDataAndContent.header)
+            post[attr] = postDataAndContent.header[attr];
+    }
+    post.last_modification_date = await getFileLastModificationDate(post.markdown_file_path);
+
+    // and we convert the markdown file to an html file
+    const destHtmlFileName = postId + '.html';
+    const html = new showdown.Converter({ strikethrough: true, extensions: ['gallery'] }).makeHtml(fileContent);
+    await writeFile(this.destHtmlFolder + destHtmlFileName, html);
+    post.html_file = destHtmlFileName;
+
+    // save the new post into the posts container
+    this.posts[postId] = post;
+}
+
+function extractHeaderFromPost(postContent) {
+    let header = postContent.match(/{(.|\n)+}\n\n/);
+    if (!header || !header[0]) {
+        console.warn('No header found.');
+        return {};
+    }
+
+    postContent = postContent.substr(header[0].length);
+
+    try {
+        return {
+            header: JSON.parse(header[0].replace(/\n/g, '')),
+            content: postContent
+        };
+    }
+    catch (e) {
+        console.error('JSON parse error !')
+        return {};
+    }
+}
+
+async function getFileLastModificationDate(filePath) {
+    const stat = await fileStat(filePath);
+    return stat.mtime;
+}
---
+++ src/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const Raven = require('raven');
+const twig = require('twig');
+
+const config = require('../config');
+const converter = new (require('./modules/converter'))(config.markdownFolder, config.postsFolder);
+
+process.on('uncaughtException', (e) => {
+	console.error(e);
+});
+process.on('unhandledRejection', (e) => {
+	console.error(e);
+});
+
+// configure Raven to report error if Sentry endpoint is specified
+if(config.sentryEndpoint)
+	Raven.config(config.sentryEndpoint, {
+		shouldSendCallback: (data) => {
+			return process.env.NODE_ENV == 'production'
+		}
+	}).install();
+
+// create the express app
+const app = express();
+twig.cache(process.env.NODE_ENV == 'production');
+app.set('views', config.postsFolder);
+
+// define the server routes
+app.get('/', (req, res, next) => {
+	res.redirect('/nz');
+});
+app.get('/:name', async (req, res, next) => {
+	const post = await converter.getPost(req.params.name);
+	if(!post)
+		return res.send('Comme tu peux voir, il n\'y a rien ici...')
+	
+	res.render(config.templatesFolder + post.post_template + '_post.twig', post);
+});
+
+if(process.env.NODE_ENV == 'test')
+	module.exports = app; // for unit tests, only the express app is required, no need to run the server
+else {
+	(async () => {
+		// convert the markdown files into html files
+		await converter.convertFiles();
+
+		// define the static folder for static assets delivering
+		app.use('/static', express.static(config.staticFolder));
+
+		// run the server on the port defined the config file
+		console.log('Server started on port ' + config.serverPort + ', waiting for requests...');
+		app.listen(config.serverPort);
+	})();
+}
