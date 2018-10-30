bb62c632d506e3dc3570e90ef2d54afc9ad31dec
Nicolas <nico@cooya.fr>
2018-10-30 18:11:20 +1300

    url and picture widths passed by the markdown

--- src/modules/converter.js
+++ src/modules/converter.js
@@ -3,7 +3,6 @@ const path = require('path');
 const showdown = require('showdown');
 const util = require('util');
 
-const config = require('../../config');
 const gallery = require('./gallery_extension');
 
 const readDir = util.promisify(fs.readdir);
@@ -16,20 +15,19 @@ module.exports = class Converter {
         this.srcMarkdownFolder = srcMarkdownFolder;
         this.destHtmlFolder = destHtmlFolder;
         this.posts = {};
-        gallery.load(config.picturesFolderUrl, config.pictureWidths);
     }
 
     async getPost(postId) {
         // get the post object
         const post = this.posts[postId];
-        if(!post)
+        if (!post)
             return null;
         //console.debug(post);
-        
+
         // check the save modification date with the current file modification date 
         const lastModificationDate = await getFileLastModificationDate(post.markdown_file_path);
         //console.debug(lastModificationDate, post.last_modification_date);
-        if(lastModificationDate.getTime() != post.last_modification_date.getTime())
+        if (lastModificationDate.getTime() != post.last_modification_date.getTime())
             await convertFile.call(this, postId + '.md'); // refresh the html file
 
         return post;
@@ -47,21 +45,21 @@ async function convertFile(fileName) {
 
     // we create a new post object
     const postId = fileName.replace('.md', '');
-    const post = {markdown_file_path: this.srcMarkdownFolder + fileName};
+    const post = { markdown_file_path: this.srcMarkdownFolder + fileName };
 
     // we read the file, extract its header and save it into the post object
     let fileContent = (await readFile(post.markdown_file_path)).toString();
     const postDataAndContent = extractHeaderFromPost(fileContent);
     if (postDataAndContent.header) {
         fileContent = postDataAndContent.content; // file content whithout header
-        for(let attr in postDataAndContent.header)
+        for (let attr in postDataAndContent.header)
             post[attr] = postDataAndContent.header[attr];
     }
     post.last_modification_date = await getFileLastModificationDate(post.markdown_file_path);
 
     // and we convert the markdown file to an html file
     const destHtmlFileName = postId + '.html';
-    const html = new showdown.Converter({ strikethrough: true, extensions: ['gallery'] }).makeHtml(fileContent);
+    const html = createConverter(post).makeHtml(fileContent);
     await writeFile(this.destHtmlFolder + destHtmlFileName, html);
     post.html_file = destHtmlFileName;
 
@@ -93,4 +91,21 @@ function extractHeaderFromPost(postContent) {
 async function getFileLastModificationDate(filePath) {
     const stat = await fileStat(filePath);
     return stat.mtime;
+}
+
+function createConverter(postData) {
+    let options = {
+        strikethrough: true
+    };
+
+    if (postData.enable_gallery) {
+        if (!postData.pictures_folder_url)
+            throw Error('If gallery enabled, the attribute "pictures_folder_url" must be specified.');
+        if (!postData.picture_widths)
+            throw Error('If gallery enabled, the attribute "picture_widths" must be specified.');
+
+        options.extensions = [gallery(postData.pictures_folder_url, postData.picture_widths)];
+    }
+
+    return new showdown.Converter(options);
 }
--- src/modules/gallery_extension.js
+++ src/modules/gallery_extension.js
@@ -1,40 +1,38 @@
 const showdown = require('showdown');
 
-module.exports = {
-    load: (picturesFolderUrl, pictureWidths) => {
-        showdown.extension('gallery', () => {
-            return [
-                { 
-                    type: 'output',
-                    regex: /<img src=".+" alt="([^\"]+)" \/>/gi,
-                    replace: function(s, alt) {
-                        let res = s.substring(0, s.length - 2) + 'title="' + alt + '">';
-                        return res;
-                    }
-                },
-                { 
-                    type: 'output',
-                    regex: /<img src="([^ ]+)".+>/gi,
-                    replace: function(s, url) {
-                        let data = '';
-                        if(url.match(/^[^\/]+(.jpg|.jpeg|.png)/)) { // only the filename is provided
-                            url = picturesFolderUrl + url;
-                            s = s.replace(/src="[^ ]+"/, 'src="' + url.replace('.jpg', '-thumbnail.jpg') + '"');
-                            for(let width of pictureWidths)
-                                data += url.replace('.jpg', '-' + width + '.jpg') + ' ' + width + ', ';
-                        }
-                        //return '<a href="' + url + '" data-responsive="' + data + '">' + s + '</a>'; // old fashion
-                        return '<div class="item" data-src="' + url + '" data-responsive="' + data + '">' + s + '</div>'
-                    }
-                },
-                {
-                    type: 'output',
-                    regex: /<p>(<div class="item" ((?!<p>).|\n)+)<\/p>/gi,
-                    replace: function(s, match) {
-                        return '<div class="lightgallery">' + match + '</div>';
-                    }
+module.exports = (picturesFolderUrl, pictureWidths) => {
+    return () => {
+        return [
+            {
+                type: 'output',
+                regex: /<img src=".+" alt="([^\"]+)" \/>/gi,
+                replace: function (s, alt) {
+                    let res = s.substring(0, s.length - 2) + 'title="' + alt + '">';
+                    return res;
                 }
-            ];
-        });
-    }
+            },
+            {
+                type: 'output',
+                regex: /<img src="([^ ]+)".+>/gi,
+                replace: function (s, url) {
+                    let data = '';
+                    if (url.match(/^[^\/]+(.jpg|.jpeg|.png)/)) { // only the filename is provided
+                        url = picturesFolderUrl + url;
+                        s = s.replace(/src="[^ ]+"/, 'src="' + url.replace('.jpg', '-thumbnail.jpg') + '"');
+                        for (let width of pictureWidths)
+                            data += url.replace('.jpg', '-' + width + '.jpg') + ' ' + width + ', ';
+                    }
+                    //return '<a href="' + url + '" data-responsive="' + data + '">' + s + '</a>'; // old fashion
+                    return '<div class="item" data-src="' + url + '" data-responsive="' + data + '">' + s + '</div>'
+                }
+            },
+            {
+                type: 'output',
+                regex: /<p>(<div class="item" ((?!<p>).|\n)+)<\/p>/gi,
+                replace: function (s, match) {
+                    return '<div class="lightgallery">' + match + '</div>';
+                }
+            }
+        ];
+    };
 };
