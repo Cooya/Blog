73d5d1c09590ba9aef941a48ce9c3f3ac6867353
Nicolas <nico@cooya.fr>
2018-11-01 14:27:32 +1300

    improvements in mobile display + back button

--- src/modules/gallery_extension.js
+++ src/modules/gallery_extension.js
@@ -23,14 +23,14 @@ module.exports = (picturesFolderUrl, pictureWidths) => {
                             data += url.replace('.jpg', '-' + width + '.jpg') + ' ' + width + ', ';
                     }
                     //return '<a href="' + url + '" data-responsive="' + data + '">' + s + '</a>'; // old fashion
-                    return '<div class="item" data-src="' + url + '" data-responsive="' + data + '">' + s + '</div>'
+                    return '<div class="item column is-one-quarter-desktop is-one-third-tablet is-half-mobile" data-src="' + url + '" data-responsive="' + data + '">' + s + '</div>'
                 }
             },
             {
                 type: 'output',
-                regex: /<p>(<div class="item" ((?!<p>).|\n)+)<\/p>/gi,
+                regex: /<p>(<div class="[a-z0-9\- ]+" ((?!<p>).|\n)+)<\/p>/gi,
                 replace: function (s, match) {
-                    return '<div class="lightgallery">' + match + '</div>';
+                    return '<div class="lightgallery columns is-centered is-multiline is-mobile is-variable is-1">' + match + '</div>';
                 }
             }
         ];
