ac42375be824150452508e20fb6d909654f37913
Nicolas <nico@cooya.fr>
2019-02-07 13:31:32 +1300

    rename-pics command created

---
+++ scripts/rename_pics.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const dateformat = require('dateformat');
+const exif = require('exif-reader');
+const sharp = require('sharp');
+
+const readDir = util.promisify(fs.readdir);
+const rename = util.promisify(fs.rename);
+
+const CURRENT_FOLDER = '.';
+
+(async () => {
+	const files = await readDir(CURRENT_FOLDER);
+	await asyncForEach(files, async (fileName) => {
+		let extension = path.extname(fileName).toLowerCase();
+		if (['.jpg', '.jpeg', '.png'].indexOf(extension) == -1) {
+			//console.warn('File "' + fileName + '" ignored.');
+			return;
+		}
+
+		let filePath = path.resolve(CURRENT_FOLDER, fileName);
+		let metadata = await sharp(filePath).metadata();
+		if (metadata.exif) {
+			let exifData = exif(metadata.exif);
+			if (exifData.image.ModifyDate) {
+				//console.debug(exifData.image.ModifyDate);
+				let newFilePath = path.resolve(CURRENT_FOLDER, convertDate(exifData.image.ModifyDate) + extension);
+				await rename(filePath, newFilePath);
+				console.log(newFilePath);
+			}
+		}
+	});
+})();
+
+async function asyncForEach(array, callback) {
+	for (let i = 0; i < array.length; i++) await callback(array[i], i, array);
+}
+
+function convertDate(date) {
+	return dateformat(new Date(date), 'dd-mm-yyyy HH:MM:ss', true);
+}
